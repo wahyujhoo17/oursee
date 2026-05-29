2b69e98aed521d6f3f4001b671aea5b5bc8ceaed
diianekawti <diianekawti@gmail.com>
2026-05-30 00:47:40 +0700

    feat: enable trustHost in auth configuration

--- auth.config.ts
+++ auth.config.ts
@@ -3,6 +3,7 @@ import type { NextAuthConfig } from "next-auth";
 export const authConfig = {
   providers: [],
   secret: process.env.AUTH_SECRET,
+  trustHost: true,
   pages: {
     signIn: "/fn-admin/login",
   },
